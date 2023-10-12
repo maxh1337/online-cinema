cf6f66786652750a1d3568f38779c1436ab9250a
maxh1337 <max11052003@yandex.ru>
2023-10-13 00:43:04 +0300

    Supress ts-ignore

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { cors: true })
 	app.setGlobalPrefix('api')
-	await app.listen(4200)
+	await app.listen(process.env.PORT, '0.0.0.0') //was 4200 only
 }
 
 bootstrap()
